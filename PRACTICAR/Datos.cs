6add5fe277ec9fa9b4ec1611d19963ee5faf1c06
Samuel-Fernandez-140923 <samueldukipkoko@gmail.com>
2024-11-19 01:35:12 -0500

    Primera salvada

---
+++ PRACTICAR/Datos.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PRACTICAR
+{
+    internal class Datos
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        public string Curso { get; set; }
+        public int n { get; set; }
+        public int na { get; set; }
+        public int ne{ get; set; }
+        public int ni { get; set; }
+        public int f { get; set; }
+
+
+
+
+        public Datos(string nombre, string apellidos, int edad, string curso,int n1,int n2,int n3,int n4,int f)
+        {
+            Nombre = nombre;
+            Apellidos = apellidos;
+            Edad = edad;
+            Curso = curso;
+           
+
+            
+        }
+
+        public static void DatosA(List<Datos> citas)
+        {
+         
+            Console.WriteLine("Ingrese los nombres del estudiante");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese los apellidos del estudiante");
+            string apellidos = Console.ReadLine();
+            Console.WriteLine("Ingrese la edad del estudiante");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el curso:");
+            string curso = Console.ReadLine();
+            Console.WriteLine("Ingrese primera nota");
+            int n1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la segunda nota");
+            int n2=int.Parse(Console.ReadLine());   
+            Console.WriteLine("Ingrese la tercera nota");
+            int n3=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la cuarta nota");
+            int n4=int.Parse(Console.ReadLine());
+           
+           
+            int f= (n1 + n2 + n3 + n4) / 4; ;
+            Console.WriteLine("EL PROMEDIO ES:"+f);
+            Datos cita = new Datos(nombre, apellidos, edad, curso,n1,n2,n3,n4,f);
+            citas.Add(cita);
+           
+            
+
+
+
+
+
+        }
+       
+      
+    }
+}
---
+++ PRACTICAR/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PRACTICAR;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Datos> citas = new List<Datos>();
+        int opcion;
+
+        do
+        {
+            
+            Console.WriteLine("Selecione:");
+            Console.WriteLine("1 para crear datos del estudiante");
+
+            Console.WriteLine("2 para finalizar");
+            opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
+            {
+                case 1:
+                    Datos.DatosA(citas);
+                break;
+                case 2:
+                   break;
+                  
+
+
+            }
+        }while (opcion != 2);
+    }
+
+}
